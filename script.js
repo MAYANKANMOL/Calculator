11baacd57d4a59dbf3fb5366286d8dc72f91b350
MAYANKANMOL <105606874+MAYANKANMOL@users.noreply.github.com>
2023-03-18 00:26:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+var displays = document.getElementById("display");
+var btns= document.getElementsByClassName("btn");
+var operand1 = null;
+var operator = null;
+var operand2 = null;
+
+
+for(var i=0; i<btns.length; i++){
+    btns[i].addEventListener('click',function(){
+        var value = this.getAttribute('data-value');
+        console.log(value);
+        if(value=='+'){
+            operator = '+';
+            operand1 = parseFloat(displays.innerText);
+            displays.innerText='';//to clear the screen or displays.textContent='';
+        }
+        else if(value=='-'){
+            operator = '-';
+            operand1 = parseFloat(displays.innerText);
+            displays.innerText='';
+        }
+        else if(value=='*'){
+            operator = '*';
+            operand1 = parseFloat(displays.innerText);
+            displays.innerText='';
+        }
+        else if(value=='/'){
+            operator = '/';
+            operand1 = parseFloat(displays.innerText);
+            displays.innerText='';
+            
+        }
+        else if(value=='%'){
+            operator = '%';
+            operand1 = parseFloat(displays.innerText);
+            displays.innerText='';
+        }
+        else if(value=='='){
+            operand2 = parseFloat(displays.innerText);
+            displays.innerText='';
+            var ans = eval(operand1 + " " + operator + " " + operand2);
+            // if(ans==NaN){
+            //     displays.innerText = 'UNDEFINED';
+            // }
+            // else{
+                displays.innerText = ans;
+            // }
+        }
+        else if(value=='DEL'){
+            var text = displays.textContent;
+            var updatedText = text.substring(0, text.length-1);
+            displays.textContent = updatedText;
+        }
+        else if(value=='AC'){
+            displays.innerText='';
+            operator=null;
+            operand1=null;
+            operand2=null;
+        }
+        else{
+            displays.textContent += value;
+        }
+    });
+    
+}
